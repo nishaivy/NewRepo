9cde6e41fd11c5cc093ad3ba93829fb63e1bd772
nishaivy <113030466+nishaivy@users.noreply.github.com>
2022-09-26 22:15:34 +0530

    add new code

--- Ques_3/Ques_3/Program.cs
+++ Ques_3/Ques_3/Program.cs
@@ -1,30 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GenericsDemo
+namespace ListCollectionDemo
 {
-    class Student
+    public class Program
     {
-       
-        static void Main()
+        public static void Main()
         {
-            //Creating a Generic List 
-            List<string> Student = new List<string> {
-            
-             "Sravan",
-            "deepu",
-             "manoja",
-            "Sathwik",
-             "Saran"
-        };
-           
-            Console.WriteLine("Accessing Generic List Elemenst");
-            foreach (var item in Student)
-            {
-                Console.WriteLine(item);
-            }
+            // Create Employee Objects
+            Employee emp1 = new Employee() { ID = 101, Name = "Pranaya" };
+            Employee emp2 = new Employee() { ID = 101, Name = "Nisha" };
+            Employee emp3 = new Employee() { ID = 102, Name = "Priyanka"};
+            Employee emp4 = new Employee() { ID = 103, Name = "Anurag"};
+            Employee emp5 = new Employee() { ID = 104, Name = "Sambit"};
 
+            // Create a List of Employees
+            List<Employee> listEmployees = new List<Employee>();
+
+            //Adding Employees to the collection using Add Method
+            listEmployees.Add(emp1);
+            listEmployees.Add(emp2);
+            listEmployees.Add(emp3);
+            listEmployees.Add(emp4);
+            listEmployees.Add(emp5);
+
+
+            Console.WriteLine("\nRetrieving All Employees using For Loop");
+            for (int i = 0; i < listEmployees.Count; i++)
+            {
+                Employee employee = listEmployees[i];
+                Console.WriteLine($"Name = {employee.Name}");
+            }
             Console.ReadKey();
         }
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
     }
 }
