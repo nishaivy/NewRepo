60b7e16e05b1b0cc5ec2cb600612b2cebf9ba4dc
nishaivy <113030466+nishaivy@users.noreply.github.com>
2022-09-27 22:36:05 +0530

    assigments

---
+++ copy_file/copy_file/Program.cs
@@ -0,0 +1,27 @@
+﻿// C# program to copy data from one file to another
+//Write a C# program to copy the contents of one file to another file and move that file to a different location.
+
+using System;
+using System.IO;
+
+class Copy_File
+{
+
+    static void Main()
+    {
+
+        // Copy contents from file1 to file2
+        File.Copy("nisha.txt", "28.txt");
+
+        // Display file2 contents
+        Console.WriteLine(File.ReadAllText("28.txt"));
+
+        string Oldfilepath = @"C:\Users\nisha.hiremani\source\repos\day12\copy_file\copy_file\bin\Debug\nisha.txt";
+        string NewFilePath = @"C:\Users\nisha.hiremani\source\repos\day12\copy_file\copy_file\28.txt";
+        StreamReader reader = new StreamReader(Oldfilepath);
+        string fileContent = reader.ReadToEnd();
+
+        StreamWriter writer = new StreamWriter(NewFilePath);
+        writer.Write(fileContent);
+    }
+}
---
+++ directory_exists/directory_exists/Program.cs
@@ -0,0 +1,27 @@
+﻿// For File.Exists, Directory.Exists  
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System;
+
+public class DirectoryFile
+{
+    public static void Main(string[] args)
+    {
+        string fileName = "C:\\Users\\nisha.hiremani\\source\\repos\\day12\\copy_file\\copy_file\\nisha29.txt";
+        if (File.Exists(fileName))
+        {
+            Console.WriteLine("File exists."); 
+        }
+
+        else if (File.Exists("C:\\Users\\nisha.hiremani\\source\\repos\\day12\\copy_file\\copy_file\\nisha21.txt"))
+        {
+            Console.WriteLine("The file exists in a directory");
+        }
+        //After that check whether the file exists in a directory or not.  
+     else { 
+            Console.WriteLine("File does not exist.");
+
+        }
+    }
+}
