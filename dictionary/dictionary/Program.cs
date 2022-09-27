1a08d5f5e689e7f37fae8975544b8b1feb875c06
nishaivy <113030466+nishaivy@users.noreply.github.com>
2022-09-27 21:02:47 +0530

    assignment

---
+++ dictionary/dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿// C# program to listing the files in a directory
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class dict
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("name: ");
+        string Username = (Console.ReadLine());
+        string fileName = Username + ".txt";
+      
+        try
+        {
+            //Pass the filepath and filename to the StreamWriter Constructor
+            //string path = System.IO.Path.Combine(@"C:\Users\nisha.hiremani\source\repos\day12\dictionary", fileName);
+            StreamWriter sw = new StreamWriter("C:\\Users\\nisha.hiremani\\source\\repos\\day12\\dictionary\\"+fileName+".txt");
+            //Write a line of text
+
+            Console.Write("age: ");
+            sw.WriteLine(Console.ReadLine());
+           
+            Console.Write("Place of origin:");
+            sw.WriteLine(Console.ReadLine());
+
+            Console.Write("Languages:");
+            sw.WriteLine(Console.ReadLine());
+            //Close the file
+            sw.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally wriiten the data.");
+        }
+
+    }
+}
